0da3638c5cf8677370f3773af97dcfddf0ce95ec
Dongliang Yu <yudl.nju@gmail.com>
2014-05-13 21:31:23 +0800

    start my chef journey

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'njuchef'
+client_key               '/home/nju/chef-repo/.chef/njuchef.pem'
+validation_client_name   'chef-validator'
+validation_key           '/home/nju/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://master.example.com:443'
+syntax_check_cache_path  '/home/nju/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/home/nju/chef-repo/cookbooks' ]
